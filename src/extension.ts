034481defe83098515a5c624c0cab737a7e7ced1
AdrianWilczynski <adrian.k.wilczynski@gmail.com>
2019-04-07 13:46:09 +0200

    Use setContext api to hide commands. Check if file is saved to disk by looking at it's uri scheme.

--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,32 @@
 import * as vscode from 'vscode';
 import { goToView } from './goToView';
-import { goToController } from './goToController';
+import { goToController, isView } from './goToController';
 import { addView } from './addView';
-import { goToPage, goToPageModel } from './goToPage';
+import { goToPage, goToPageModel, isPage, isPageModel } from './goToPage';
+import { isController } from './view';
 
 export function activate(context: vscode.ExtensionContext) {
+    setContext(vscode.window.activeTextEditor);
+
     context.subscriptions.push(
         vscode.commands.registerCommand('extension.goToView', goToView),
         vscode.commands.registerCommand('extension.addView', addView),
         vscode.commands.registerCommand('extension.goToController', goToController),
         vscode.commands.registerCommand('extension.goToPage', goToPage),
-        vscode.commands.registerCommand('extension.goToPageModel', goToPageModel));
+        vscode.commands.registerCommand('extension.goToPageModel', goToPageModel),
+
+        vscode.window.onDidChangeActiveTextEditor(setContext));
 }
 
 export function deactivate() { }
+
+function setContext(editor: vscode.TextEditor | undefined) {
+    if (!editor || editor.document.isUntitled || editor.document.uri.scheme !== 'file') {
+        return;
+    }
+
+    vscode.commands.executeCommand('setContext', 'isPage', isPage(editor.document.fileName));
+    vscode.commands.executeCommand('setContext', 'isPageModel', isPageModel(editor.document.fileName));
+    vscode.commands.executeCommand('setContext', 'isController', isController(editor.document.fileName));
+    vscode.commands.executeCommand('setContext', 'isView', isView(editor.document.fileName));
+}
